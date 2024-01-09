de0260a5ecea2cd4909e0624836f0931fe090764
Brian Howell <brianhowell@pm.me>
2024-01-09 17:31:33 -0500

    addMovie

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -12,6 +12,15 @@ const reducer = (state = initialState, action) => {
       return {
         movies: state.movies.filter((item) => action.payload !== item.id),
       };
+    case ADD_MOVIE: {
+      const newMovie = {
+        id: state.movies.length + 1,
+        ...action.payload,
+      };
+      return {
+        movies: [...state.movies, newMovie],
+      };
+    }
     default:
       return state;
   }
